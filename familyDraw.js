cb712d63bb409233bb52fdc37849c0dce049231b
bo johnson <bo@letterx.co>
2019-11-25 13:00:10 +0000

    version 2 + gitignore

--- familyDraw.js
+++ familyDraw.js
@@ -1,68 +1,32 @@
-var gk = ["Dad", "Mom"];
-var bn = ["bo", "nat"];
-var jb = ["John", "Bri"];
-var zb = ["Zak", "Bekah"];
-var mh = ["Mike", "Hannah"];
+function download(content, fileName, contentType) {
+  var a = document.createElement("a");
+  var file = new Blob([content], {type: contentType});
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+let testData = "Hello, look I'm a message.";
 
-allParticipantsArray = [gk, bn, jb, zb, mh];
-allParticipants = [].concat.apply([], allParticipantsArray);
-
-// console.log(allParticipants);
-
-var drawers = allParticipants;
-var drawees = allParticipants;
-
-function shuffleFamily(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var swapPosition = Math.floor( Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[swapPosition];
-        array[swapPosition] = temp;
-    }
+function addName() {
+  let name1 = document.getElementById("name1");
+  let name2 = document.getElementById("name2");
+  app.names.push([name1.value, name2.value]);
+  app.names.push([name2.value, name1.value]);
+  name1.value = "";
+  name2.value = "";
 }
 
-shuffleFamily(allParticipants);
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: "Hello x",
+    names: []
+  }
+})
 
-// console.log(allParticipants);
+function updated() {
 
-function testPair (one, two) {
-    var result = true;
-    for (var i = 0; i < allParticipantsArray.length; i++) {
-        if (allParticipantsArray[i].indexOf(one) >= 0 && allParticipantsArray[i].indexOf(two) >= 0) {
-            result = false;
-        }
-    }
-    return result;
 }
-
-function testDraws(array) {
-    var result = [];
-    for (var i = 0; i < array.length; i++) {
-        var nameOne = array[i];
-        var nameTwo = array[i+1];
-        if (i === array.length - 1) {
-            nameTwo = array[0];
-        }
-        result[i] = testPair(nameOne,nameTwo);
-        // console.log("Testing " + nameOne + " with " + nameTwo + ": " + result[i]);
-    }
-    return !(result.indexOf(false) >= 0);
+function area() {
+  console.log("area'd");
 }
-
-var successfulDraw = false;
-var iterations = 0;
-while (!successfulDraw) {
-    iterations++;
-    shuffleFamily(allParticipants);
-    successfulDraw = testDraws(allParticipants);
-}
-console.log(iterations, allParticipants);
-
-var txtFile = "~/test.txt";
-var file = new File(txtFile);
-var string = "testing 123";
-
-file.open("w");
-file.writeIn("Another thing to test");
-file.write(str);
-file.close();
